41f389c241ce2ceef9bdd941caf7977b26fad14f
Naim <jimkhannaim97@gmail.com>
2024-09-27 23:07:52 +0600

    modified

--- Lab_Test.dart
+++ Lab_Test.dart
@@ -1,12 +1,22 @@
-class labTest{
-  var myName="Naimur Rahman";
-  displayNumber(int a, int b){
-    int c=a+b;
-    print(c);
+class Car {
+  // Public properties
+  String brand;
+  String model;
+  int year;
+
+  Car(this.brand, this.model, this.year);
+
+  int carAge() {
+    int presentYear=2024;
+    return presentYear - year;
   }
 }
-void main(){
-  labTest obj =new labTest();
-  print(obj.myName);
-  obj.displayNumber(10, 20);
+
+void main() {
+  Car car = Car('Toyota', 'Corolla', 2015);
+
+  print('Brand: ${car.brand}');
+  print('Model: ${car.model}');
+  print('Year: ${car.year}');
+  print('Car Age: ${car.carAge()} years');
 }
